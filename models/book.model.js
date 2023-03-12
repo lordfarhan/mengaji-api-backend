66df25416d9927153c9264fa493e58e475dfe0af
Muhammad Farhan <farhan1205muhammad@gmail.com>
2023-03-12 16:33:13 +0700

    Fix book model

--- models/book.model.js
+++ models/book.model.js
@@ -2,27 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Book = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  authorYearOfDeath: {
-    type: Number,
-    required: true,
-  },
-  tags: [
-    {
-      type: String,
-    }
-  ],
-  publisher: {
-    type: String,
-    required: true,
-  }
+	title: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: String,
+		required: true,
+	},
+	authorYearOfDeath: {
+		type: Number,
+		required: true,
+	},
+	tags: [
+		{
+			type: String,
+		},
+	],
+	publishers: [
+		{
+			type: String,
+		},
+	],
 });
 
 module.exports = mongoose.model("Book", Book);
