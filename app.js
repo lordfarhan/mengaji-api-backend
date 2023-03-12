8890e6d8f53bcf66a3c2221d770394125970d291
Muhammad Farhan <farhan1205muhammad@gmail.com>
2023-03-12 17:47:10 +0700

    Setup environtment

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
@@ -12,15 +15,17 @@ app.use(cors());
 
 app.use("/books", require("./routes/book.route"));
 
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI;
 mongoose.set("strictQuery", false);
 mongoose
-	.connect("mongodb://127.0.0.1:27017/mengaji", {
+	.connect(mongoUri, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
 	.then((result) => {
-		app.listen(3000);
-		console.log("connected: " + 3000);
+		app.listen(port);
+		console.log("connected: " + port);
 	})
 	.catch((error) => {
 		console.log("error connecting mongodb: " + error.message);
