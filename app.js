f44b87d221df609534e32887433bf7b3216d7f9a
Muhammad Farhan <farhan1205muhammad@gmail.com>
2023-03-12 17:39:04 +0700

    Forge app

--- app.js
+++ app.js
@@ -10,16 +10,18 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.use("/books", require("./routes/book.route"));
+
 mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mengaji", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) => {
-    app.listen(80);
-    console.log("connected: " + 80);
-  })
-  .catch((error) => {
-    console.log("error connecting mongodb: " + error.message);
-  });
+	.connect("mongodb://127.0.0.1:27017/mengaji", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((result) => {
+		app.listen(3000);
+		console.log("connected: " + 3000);
+	})
+	.catch((error) => {
+		console.log("error connecting mongodb: " + error.message);
+	});
